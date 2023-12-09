977c865ae0a209722f3305819cf1d07ee2b76657
Nanaka <nanaka.kumiho@gmail.com>
2023-12-09 06:18:57 +0100

    Adding ScrollableTabRow to select the work type in the library

--- app/src/main/java/fr/jaetan/jmedia/app/library/LibraryViewModel.kt
+++ app/src/main/java/fr/jaetan/jmedia/app/library/LibraryViewModel.kt
@@ -1,10 +1,13 @@
 package fr.jaetan.jmedia.app.library
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import fr.jaetan.jmedia.core.models.WorkType
 
 class LibraryViewModel: ViewModel() {
     var showWorkTypeSelectorSheet by mutableStateOf(false)
+    var currentLibrary by mutableStateOf(WorkType.all[0])
 }
--- app/src/main/java/fr/jaetan/jmedia/app/library/views/LibrarySubviews.kt
+++ app/src/main/java/fr/jaetan/jmedia/app/library/views/LibrarySubviews.kt
@@ -1,5 +1,7 @@
 package fr.jaetan.jmedia.app.library.views
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.LibraryBooks
@@ -7,17 +9,29 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import fr.jaetan.jmedia.R
 import fr.jaetan.jmedia.app.library.LibraryView
 import fr.jaetan.jmedia.app.work_type_choice.WorkTypeChoiceView
 import fr.jaetan.jmedia.core.extensions.isNotNull
+import fr.jaetan.jmedia.core.models.WorkType
 import fr.jaetan.jmedia.core.services.MainViewModel
 import fr.jaetan.jmedia.core.services.Navigator
 import fr.jaetan.jmedia.ui.widgets.JBottomSheet
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.selects.select
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -45,14 +59,28 @@ fun LibraryView.BottomSheetView() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LibraryView.TopBarView() {
-    TopAppBar(
-        title = {},
-        actions = {
-            IconButton(onClick = { viewModel.showWorkTypeSelectorSheet = true }) {
-                Icon(Icons.Default.LibraryBooks, null)
+    val implementedWorkTypes = WorkType.all
+
+
+    Column {
+        TopAppBar(
+            title = {
+                Text(text = stringResource(R.string.library))
+            }
+        )
+        ScrollableTabRow (
+            selectedTabIndex = WorkType.all.indexOf(viewModel.currentLibrary),
+            edgePadding = 0.dp
+        ) {
+            implementedWorkTypes.forEach { workType ->
+                Tab(
+                    selected = workType == viewModel.currentLibrary,
+                    onClick = { viewModel.currentLibrary = workType },
+                    text = { Text(stringResource(id = workType.textRes)) }
+                )
             }
         }
-    )
+    }
 }
 
 @Composable
