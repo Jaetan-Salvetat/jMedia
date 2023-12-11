05222941c8cc012989b7ae5a0fe55302e2384f7d
Nanaka <nanaka.kumiho@gmail.com>
2023-12-11 20:03:18 +0100

    ObjectBox Installed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath("io.objectbox:objectbox-gradle-plugin:3.7.1")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
