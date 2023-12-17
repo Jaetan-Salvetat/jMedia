090c7375f667e60f5127839f6965fc18bd827036
Jaetan-Salvetat <gaetansalvi08@gmail.com>
2023-12-18 00:03:20 +0100

    Remove WorkChoice bottom sheet

--- app/src/main/java/fr/jaetan/jmedia/app/library/LibraryView.kt
+++ app/src/main/java/fr/jaetan/jmedia/app/library/LibraryView.kt
@@ -1,12 +1,8 @@
 package fr.jaetan.jmedia.app.library
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-import fr.jaetan.jmedia.app.library.views.BottomSheetView
 import fr.jaetan.jmedia.app.library.views.FabView
 import fr.jaetan.jmedia.app.library.views.TopBarView
-import fr.jaetan.jmedia.core.extensions.isNull
-import fr.jaetan.jmedia.core.services.MainViewModel
 import fr.jaetan.jmedia.ui.Screen
 
 class LibraryView: Screen<LibraryViewModel>() {
@@ -21,21 +17,7 @@ class LibraryView: Screen<LibraryViewModel>() {
     override fun Content() = Unit
 
     @Composable
-    override fun BottomSheet() {
-        BottomSheetView()
-    }
-
-    @Composable
     override fun Fab() {
         FabView()
-    }
-
-    @Composable
-    override fun Initialize(nc: NavHostController?) {
-        super.Initialize(nc)
-
-        if (MainViewModel.state.currentWorkType.isNull()) {
-            viewModel.showWorkTypeSelectorSheet = true
-        }
     }
 }
--- app/src/main/java/fr/jaetan/jmedia/app/library/LibraryViewModel.kt
+++ app/src/main/java/fr/jaetan/jmedia/app/library/LibraryViewModel.kt
@@ -1,10 +1,7 @@
 package fr.jaetan.jmedia.app.library
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class LibraryViewModel: ViewModel() {
-    var showWorkTypeSelectorSheet by mutableStateOf(false)
+
 }
