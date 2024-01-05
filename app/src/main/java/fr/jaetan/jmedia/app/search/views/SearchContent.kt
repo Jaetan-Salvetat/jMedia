36ac681951e0b1294d83769df9ec72c8b55abe2e
Jaetan-Salvetat <gaetansalvi08@gmail.com>
2024-01-05 20:48:29 +0100

    Edit search list item key

--- app/src/main/java/fr/jaetan/jmedia/app/search/views/SearchContent.kt
+++ app/src/main/java/fr/jaetan/jmedia/app/search/views/SearchContent.kt
@@ -133,7 +133,7 @@ private fun SearchView.WorksList() {
     }
 
     LazyColumn(state = listState) {
-        items(viewModel.works, key = { "${it.title}/${it.type}/${it.synopsis.orEmpty()}" }) {
+        items(viewModel.works, key = { "${it.title}/${it.type}/${it.synopsis.orEmpty().ifEmpty { it.title }}" }) {
             WorksListItem(it, Modifier.animateItemPlacement())
         }
 
