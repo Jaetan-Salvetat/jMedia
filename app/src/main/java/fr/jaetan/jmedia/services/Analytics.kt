e616d58f5f536606186113345511b6d7dc84a5a1
Jaetan-Salvetat <gaetansalvi08@gmail.com>
2024-02-10 02:27:52 +0100

    Cancel analytics event if we're not in release version

--- app/src/main/java/fr/jaetan/jmedia/services/Analytics.kt
+++ app/src/main/java/fr/jaetan/jmedia/services/Analytics.kt
@@ -5,20 +5,25 @@ import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.analytics.logEvent
 import com.google.firebase.ktx.Firebase
 import fr.jaetan.jmedia.models.AnalyticsEvent
+import fr.jaetan.jmedia.models.GlobalSettings
 
 object Analytics {
     private val analytics = Firebase.analytics
 
     fun tagScreen(name: String) {
-        analytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
-            param(FirebaseAnalytics.Param.SCREEN_NAME, name)
+        if (GlobalSettings.isInRelease) {
+            analytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+                param(FirebaseAnalytics.Param.SCREEN_NAME, name)
+            }
         }
     }
 
     fun tagEvent(event: AnalyticsEvent, vararg params: Pair<String, String>) {
-        analytics.logEvent(event.name) {
-            params.forEach {
-                param(it.first, it.second)
+        if (GlobalSettings.isInRelease) {
+            analytics.logEvent(event.name) {
+                params.forEach {
+                    param(it.first, it.second)
+                }
             }
         }
     }
