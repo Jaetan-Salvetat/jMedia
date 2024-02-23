4813ab325e708c4563c63b68dd079934a0898175
Jaetan-Salvetat <gaetansalvi08@gmail.com>
2024-02-23 10:38:01 +0100

    Edit release build variant

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
         }
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
