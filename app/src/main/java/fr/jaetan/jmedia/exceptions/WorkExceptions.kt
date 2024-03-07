67e0da0ebeae6090c6f52c969d17a5f76a739423
Jaetan-Salvetat <gaetansalvi08@gmail.com>
2024-03-07 14:40:23 +0100

    Implement a linter

--- app/src/main/java/fr/jaetan/jmedia/exceptions/WorkExceptions.kt
+++ app/src/main/java/fr/jaetan/jmedia/exceptions/WorkExceptions.kt
@@ -3,4 +3,5 @@ package fr.jaetan.jmedia.exceptions
 
 import fr.jaetan.jmedia.models.works.shared.WorkType
 
-class UnknownStatusException(status: String, type: WorkType): Exception("Unknown work status (${status.ifEmpty { "empty" }}) for $type work type")
+class UnknownStatusException(status: String, type: WorkType)
+    : Exception("Unknown work status (${status.ifEmpty { "empty" }}) for $type work type")
