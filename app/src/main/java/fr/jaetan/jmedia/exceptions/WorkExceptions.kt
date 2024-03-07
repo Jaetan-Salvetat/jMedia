704ba1b57265d90f448ffd04591857db42239669
Jaetan-Salvetat <gaetansalvi08@gmail.com>
2024-03-07 15:34:30 +0100

    Edit changelog.md

--- app/src/main/java/fr/jaetan/jmedia/exceptions/WorkExceptions.kt
+++ app/src/main/java/fr/jaetan/jmedia/exceptions/WorkExceptions.kt
@@ -3,4 +3,6 @@ package fr.jaetan.jmedia.exceptions
 
 import fr.jaetan.jmedia.models.works.shared.WorkType
 
-class UnknownStatusException(status: String, type: WorkType): Exception("Unknown work status (${status.ifEmpty { "empty" }}) for $type work type")
+class UnknownStatusException(status: String, type: WorkType, test: test = test()): Exception("Unknown work status (${status.ifEmpty { "empty" }}) for $type work type")
+
+class test
