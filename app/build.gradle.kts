f7ea36dac90a46d104d2c785a512ec9a56d228e9
Jaetan-Salvetat <gaetansalvi08@gmail.com>
2024-03-11 02:54:26 +0100

    Update navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,7 +98,7 @@ dependencies {
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-graphics:$composeVersion")
     implementation("androidx.compose.material3:material3:1.2.1")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.navigation:navigation-compose:2.8.0-alpha04")
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
 
     // Networking
