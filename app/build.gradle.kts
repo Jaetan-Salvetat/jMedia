e0162ed01a5454be941e3e7a71ba25b474ba64ad
Jaetan-Salvetat <gaetansalvi08@gmail.com>
2024-05-02 14:38:05 +0200

    Upgrade gradle and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,17 +109,17 @@ android {
 }
 
 dependencies {
-    val composeVersion = "1.6.5"
+    val composeVersion = "1.6.7"
     val ackpineVersion = "0.5.1"
 
     // Androidx
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-graphics:$composeVersion")
     implementation("androidx.compose.material3:material3:1.2.1")
-    implementation("androidx.navigation:navigation-compose:2.8.0-alpha06")
+    implementation("androidx.navigation:navigation-compose:2.8.0-alpha08")
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
 
     // Networking
@@ -139,7 +139,7 @@ dependencies {
 
     // Storage
     implementation("io.realm.kotlin:library-base:1.13.0")
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 
     // Images
     implementation("io.coil-kt:coil-compose:2.6.0")
@@ -161,7 +161,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     // Others
