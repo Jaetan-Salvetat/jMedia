e417b6a1a6be4853cf10a6b83e35f42527bb259c
Jaetan-Salvetat <gaetansalvi08@gmail.com>
2024-05-11 18:02:53 +0200

    Clean code

--- app/src/main/java/fr/jaetan/jmedia/extensions/List.kt
+++ app/src/main/java/fr/jaetan/jmedia/extensions/List.kt
@@ -4,4 +4,4 @@ fun <T : Any?> List<T>.removeNullValues(): List<T & Any> = filterNotNull()
 
 fun <T : Any> List<T>.removeDuplicate(): List<T> = toSet().toList()
 
-fun <T : Any> List<T>?.isNotNullOrEmpty(): Boolean = isNotNull() && this?.isNotEmpty() ?: false
+fun <T : Any> List<T>?.isNotNullOrEmpty(): Boolean = !isNullOrEmpty()
